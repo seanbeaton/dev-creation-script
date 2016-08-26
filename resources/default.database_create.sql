5e2a22227fa6506c0044f4e97e26ba441539cb86
seanbeaton <beatonseanp@gmail.com>
2016-08-26 15:20:00 -0400

    Inital commit

---
+++ resources/default.database_create.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE TMP_DEV_DB_NAME DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+INSERT INTO mysql.db (Host, Db, User, Select_priv, Insert_priv, Update_priv, Delete_priv, Create_priv, Drop_priv, Grant_priv, References_priv, Index_priv, Alter_priv, Create_tmp_table_priv, Lock_tables_priv, Create_view_priv, Show_view_priv, Create_routine_priv, Alter_routine_priv, Execute_priv)
+VALUES ('localhost', 'TMP_DEV_DB_NAME', 'TMP_DEV_USERNAME', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'N', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y');
+FLUSH PRIVILEGES ;
